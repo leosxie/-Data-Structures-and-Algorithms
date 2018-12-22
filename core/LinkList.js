72fc818ae64ac6316575f95f8052476606820544
leosxie <leosxie@tencent.com>
2018-12-22 14:05:44 +0800

    加入链表节点值比较方法

--- core/LinkList.js
+++ core/LinkList.js
@@ -1,81 +1,96 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-/**双向节点类型**/
-var Node = /** @class */ (function () {
-    function Node(value) {
+class Node {
+    constructor(value) {
         this.data = value;
         this.next = null;
         this.prev = null;
     }
-    return Node;
-}());
+}
 exports.Node = Node;
-//双向列表
-var LinkList = /** @class */ (function () {
-    function LinkList() {
-        var datas = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            datas[_i] = arguments[_i];
-        }
-        var _this = this;
-        //哨兵检测
+class LinkList {
+    constructor(...datas) {
         this._head = null;
         this._tail = null;
         this._length = 0;
-        datas.forEach(function (data) {
-            _this._add(data);
+        this._isEqual = function (prevNode, nextNode) {
+            console.log(prevNode.data === nextNode.data);
+            return prevNode.data === nextNode.data;
+        };
+        datas.forEach((data) => {
+            this._add(data);
         });
     }
-    Object.defineProperty(LinkList.prototype, "head", {
-        get: function () {
-            return this._head.data;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    Object.defineProperty(LinkList.prototype, "tail", {
-        get: function () {
-            return this._tail.data;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    Object.defineProperty(LinkList.prototype, "lenght", {
-        get: function () {
-            return this._length;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    //在链表的后面插入数据
-    LinkList.prototype._add = function (data) {
-        var newNode = new Node(data);
-        //判断头部节点是否为空
+    get head() {
+        return this._head.data;
+    }
+    get tail() {
+        return this._tail.data;
+    }
+    get length() {
+        return this._length;
+    }
+    _add(data) {
+        let newNode = new Node(data);
         if (!this._head) {
             this._head = this._tail = newNode;
         }
         else {
-            /**
-             * 注意顺序，
-             * 新节点是插入到链表最后面的
-             * 1.把尾巴节点的引用指向新加的节点
-             * 2.把新节点的前置引用指向尾节点--节点就串起来了
-             * 3.本地变量表示最后一个节点就是先加的节点
-             *
-             * **/
-            //把尾巴连起来
             this._tail.next = newNode;
-            //节点闭环 把头节点的上一个节点指向最后新加的节点
             newNode.prev = this._tail;
-            //把新家的节点作为头部节点
             this._tail = newNode;
         }
-        // 标志数据长度+1
         this._length += 1;
-    };
-    //插入一个节点
-    LinkList.prototype.insert = function (node, prev) {
-    };
-    return LinkList;
-}());
+    }
+    *iterator() {
+        let currentItem = this._head;
+        while (currentItem) {
+            yield currentItem.data;
+            currentItem = currentItem.next;
+        }
+    }
+    [Symbol.iterator]() {
+        return this.iterator();
+    }
+    insert(val, prev) {
+        let newNode = new Node(val);
+        let prevNode = new Node(prev);
+        let currentNode = this._head;
+        if (!currentNode) {
+            return false;
+        }
+        else {
+            while (true) {
+                if (this._isEqual(currentNode, prevNode)) {
+                    newNode.next = currentNode.next;
+                    newNode.prev = currentNode;
+                    currentNode.next = newNode;
+                    if (newNode.next) {
+                        newNode.next.prev = newNode;
+                    }
+                    else {
+                        this._tail = newNode;
+                    }
+                    this._length++;
+                    return true;
+                }
+                else {
+                    if (currentNode.next) {
+                        currentNode = currentNode.next;
+                    }
+                    else {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    set isEqual(func) {
+        this._isEqual = func;
+    }
+    get isEqual() {
+        return this._isEqual;
+    }
+}
 exports.default = LinkList;
+//# sourceMappingURL=LinkList.js.map
--- core/LinkList.ts
+++ core/LinkList.ts
@@ -1,4 +1,6 @@
-/**双向节点类型**/
+/**双向节点类型
+ * https://github.com/sfkiwi/linked-list-typescript/blob/master/src/index.ts
+ * **/
 export class Node<T>{
   data:T;
   next?:Node<T>;
@@ -16,6 +18,10 @@ export default class LinkList<T>{
     private _head:Node<T> = null;
     private _tail:Node<T> = null;
     private _length:number = 0;
+    private _isEqual:Function = function(prevNode:Node<T>,nextNode:Node<T>){
+        console.log(prevNode.data === nextNode.data);
+        return prevNode.data === nextNode.data;
+    };
 
     get head():T{
         return this._head.data;
@@ -23,7 +29,7 @@ export default class LinkList<T>{
     get tail():T{
         return this._tail.data;
     }
-    get lenght():number{
+    get length():number{
         return this._length;
     }
     //在链表的后面插入数据
@@ -61,10 +67,71 @@ export default class LinkList<T>{
            this._add(data);
        });
     }
-    //插入一个节点
-    insert(node:T,prev:T){
 
+    *iterator(): IterableIterator<T> {
+        let currentItem = this._head;
+
+        while(currentItem) {
+            yield currentItem.data;
+            currentItem = currentItem.next
+        }
+    }
+
+    [Symbol.iterator]() {
+        return this.iterator();
+    }
+    /**
+     * 在给定节点值后的节点插入一个数据
+     * @param val --插入的值
+     * @param prev--上一个节点的值
+     */
+    insert(val:T,prev:T){
+
+         let newNode = new Node(val);
+
+         //构建一个伪节点用于比较值，或者通过这个值去找节点
+         let prevNode = new Node(prev);
+
+         let currentNode:Node<T> = this._head;
+         //如果没有头部节点代表不没有初始化
+         if(!currentNode){
+             return false;
+         }else{
+           //用头部节点指针一直往下走，知道找到prev节点
+           while (true){
+               if( this._isEqual(currentNode,prevNode) ){
+                   newNode.next = currentNode.next;
+                   newNode.prev = currentNode;
+                   currentNode.next = newNode;
+
+                   //判断新节点后还有没有值，有可能是在节点尾部插入的
+                   if(newNode.next){
+                       newNode.next.prev = newNode;
+                   }else{
+                       this._tail = newNode;
+                   }
+                   this._length++;
+                   return true;
+               }else{
+                   if(currentNode.next){
+                       currentNode = currentNode.next;
+                   }else {
+                       return false;
+                   }
+               }
+           }
+
+         }
+
+    }
+    public set isEqual(func:Function){
+        this._isEqual = func;
+    }
+    public get isEqual(){
+        return this._isEqual;
     }
 
 
 }
+
+
